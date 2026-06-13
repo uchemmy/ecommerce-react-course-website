00fe293d54e01957a630b043536a95db71fb66bf
Emmanuel Ojigbo <emmanuel.ojigbo@fastcredit-ng.com>
2026-06-13 19:39:03 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import Checkout from "./pages/Checkout";
+import Navbar from "./components/Navbar";
+
+import "./App.css";
+
+function App() {
+  return;
+  <div className="app">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
+  </div>;
+}
+
+export default App;
